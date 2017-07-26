b41226807eedc48c3d2e104bce08de190b1fa49a
Kasser <lkasser@stjude.org>
2017-07-26 09:50:56 -0500

    commented and cleaned up the code

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,6 @@ package main;
 
 import java.awt.AWTException;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -10,42 +9,41 @@ import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-
-// (475, 225)
-// (475, 206)
-// (518, ...)
-
-// 83, 247
-
 public class Main {
 	
 	static Robot r;
 	
+	// this is the color threshold for an obstacle
 	static final int threshold = 165;
+
+	// these are the pixel coordinates for sampling the background color
 	static final int backgroundX = 165;
 	static final int backgroundY = 337;
 	
+	// the lower obstacle tracking coordinates 
 	static final int lowX = 475;
 	static final int lowY = 215;
+	
+	// the higher obstacle tracking coordinates 
 	static final int highX = 475;
 	static final int highY = 190;
+	
+	// how large do we want the obstacle detection boxes to be (the
+	// boxes are centered on the tracking coordinates)
 	static final int width = 40;
 	static final int height = 5;
 	
+	// these define the amount of time (in ms) that the buttons are held for jumps
 	static final int shortJumpTime = 100;
 	static final int duckTime = 400;
 	static final int longJumpTime = 200;
 	
+	// the current speed of the game (increases as the game progresses)
 	static int speed = 0;
-	
 
 	public static void main(String[] args) {
-
 		try {
+			// this allows us to press buttons
 			r = new Robot();
 		} catch (AWTException e) {
 			System.err.println("Could not create robot");
@@ -53,10 +51,13 @@ public class Main {
 		}
 
 		playGame();
-		//trackMouse();
-		
 	}
 	
+	/*
+	 * this is to be used to configure the tracking points- move your
+	 * mouse to where you'd like the tracking points to be and then change
+	 * their values in the code
+	 */
 	public static void trackMouse() {
 		while (true) {
 			Point pt = MouseInfo.getPointerInfo().getLocation();
@@ -74,32 +75,44 @@ public class Main {
 		}
 	}
 	
+	/*
+	 * the main method for playing the game. It will run indefinitely, even if it loses
+	 */
 	public static void playGame() {
 		
-		r.mouseMove(lowX, lowY);
 		boolean wasDay = true;
 		
 		while (true) {
 			
 			boolean isDay = isDay();
 
+			/*
+			 * if the night just changed to day, increase the speed
+			 */
 			if (!wasDay && isDay) {
 				speed = Math.min(speed + 1, 3);
 			}
-			
 			wasDay = isDay;
 
+			// first we get the high obstacle detection zone
 			BufferedImage highImg = r.createScreenCapture(new Rectangle(highX, highY, width + speed * 7, height));
 			boolean highObstical = isObstical(highImg);
 			
+			/*
+			 *  this prevents the dino from ducking when it encounters certain cacti. Consider the case where the
+			 *  high detection zone triggers because of the high part of the cactus, but the low zone doesn't because
+			 *  the center of the cactus is recessed. Now, iff the high zone triggers, the low zone looks ahead more
+			 *  in order to prevent this problem 
+			 */
 			int lowLookAhead = 0;
 			if (highObstical) {
+				lowLookAhead = 10;
 			}
-			lowLookAhead = 10;
 			
 			BufferedImage lowImg = r.createScreenCapture(new Rectangle(lowX, lowY, width + speed * 7 + lowLookAhead, height));
 			boolean lowObstical = isObstical(lowImg);
 
+			// use the results to determine whether or not to dodge
 			dodge(lowObstical, highObstical);
 			
 		}
@@ -127,6 +140,14 @@ public class Main {
 		}
 	}
 	
+	/*
+	 * here we jump
+	 * 
+	 * time is the amount of time for which to hold the up key, and
+	 * downWaitTime is the amount of time for which to wait before
+	 * pressing the down key. If downWaitTime == 0, the down key
+	 * is not pressed
+	 */
 	public static void jump(int time, int downWaitTime) {
 		r.keyPress(KeyEvent.VK_UP);
 		try {
@@ -153,6 +174,7 @@ public class Main {
 		}
 	}
 
+	// more or less same as jump
 	public static void duck(int time) {
 		r.keyPress(KeyEvent.VK_DOWN);
 		try {
@@ -164,28 +186,15 @@ public class Main {
 		r.keyRelease(KeyEvent.VK_DOWN);
 	}
 	
+	// compares the background color to the threshold in order to determine whether
+	// it's night or day
 	public static boolean isDay() {
 		return r.getPixelColor(backgroundX, backgroundY).getRed() > threshold;
 	}
 	
-	/*public static boolean isObstical(boolean isDay, BufferedImage img) {
-		boolean obstical = false;
-		for (int x = 0; x < img.getWidth() && !obstical; x++) {
-			for (int y = 0; y < img.getHeight() && !obstical; y++) {
-				if (isDay) {
-					obstical |= new Color(img.getRGB(x, y)).getRed() < threshold;
-				} else {
-					obstical |= new Color(img.getRGB(x, y)).getRed() > threshold;
-				}
-			}
-		}
-		
-		return obstical;
-	}*/
-	
+	// determines whether there's an obstacle in img
 	public static boolean isObstical(BufferedImage img) {
 		boolean obstical = false;
-		//int same = new Color(img.getRGB(0, 0)).getRed();
 		int background = r.getPixelColor(backgroundX, backgroundY).getRed();
 		for (int x = 0; x < img.getWidth() && !obstical; x++) {
 			for (int y = 0; y < img.getHeight() && !obstical; y++) {
